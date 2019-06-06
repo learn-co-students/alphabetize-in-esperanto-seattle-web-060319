798a06ced6a897c2245d17658faa96f9aef18fa7
Karen Go <karenrgo@gmail.com>
2019-06-06 16:30:33 -0700

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,16 @@
+require 'pry'
+
+ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+# ea = ESPERANTO_ALPHABET.split(//)
+
+
 def alphabetize(arr)
-  # code here
+
+	esperanto = ESPERANTO_ALPHABET
+	ascii = "@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\"
+
+	arr.sort_by! do |str|
+		str.tr(esperanto,ascii)
+	end
+
 end
